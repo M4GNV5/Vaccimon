19263d74390d7241f9f86a7d39b96df0a566f977
Alexander Horn <alexhorn@users.noreply.github.com>
2021-07-14 00:41:27 +0200

    Add bootstrap and other dependencies

--- vaccimon-app/pages/_app.tsx
+++ vaccimon-app/pages/_app.tsx
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
--- vaccimon-app/pages/index.tsx
+++ vaccimon-app/pages/index.tsx
@@ -1,12 +1,26 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import { Button, Container } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faQrcode } from '@fortawesome/free-solid-svg-icons'
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>Vaccímon</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    </div>
+      <Container>
+        <h1>Vaccimon</h1>
+        <Link href="/scan" passHref>
+          <Button>
+            <FontAwesomeIcon icon={faQrcode} />
+            {' '}
+            Scan a new Vaccimon
+          </Button>
+        </Link>
+      </Container>
+    </>
   )
 }
