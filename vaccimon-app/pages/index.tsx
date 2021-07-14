1dbbe1c47bfcbc27584581177448ac32b1438cd7
Jakob Löw <jakob@m4gnus.de>
2021-07-14 18:53:13 +0200

    :lipstick: restyle vaccimons in the vaccidex to a card-like layout

--- vaccimon-app/pages/index.tsx
+++ vaccimon-app/pages/index.tsx
@@ -1,13 +1,37 @@
 import Head from 'next/head'
-import Link from 'next/link'
 import Image from 'next/image'
-import { Badge, Button, Container, Nav, Navbar } from 'react-bootstrap'
+import { Button, Container, Nav, Navbar } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faQrcode } from '@fortawesome/free-solid-svg-icons'
+import {
+  IconDefinition,
+  faChessPawn,
+  faCrow,
+  faDna,
+  faVirus,
+  faQrcode,
+  faSyringe,
+  faCheck,
+  faTimes,
+  faFlagUsa,
+  faTint,
+} from '@fortawesome/free-solid-svg-icons'
 import VaccimonRepo from '../lib/repository'
 import { useEffect, useState } from 'react'
 import styles from '../styles/index.module.css'
 import { Vaccimon } from '../lib/vaccimon'
+
+const icons: {[key: string]: IconDefinition[]} = {
+  "Comirnaty": [faDna, faChessPawn],
+  "Spikevax": [faDna, faCrow],
+  "Vaxzevria": [faVirus, faTint],
+  "COVID-19 Vaccine Janssen": [faVirus, faFlagUsa],
+}
+const classes: {[key: string]: string} = {
+  "Comirnaty": styles["card-comirnaty"],
+  "Spikevax": styles["card-spikevax"],
+  "Vaxzevria": styles["card-vaxzevria"],
+  "COVID-19 Vaccine Janssen": styles["card-janssen"],
+}
 
 export default function Home() {
   const [vaccimon, setVaccimon] = useState<Vaccimon[]>()
@@ -28,6 +52,19 @@ export default function Home() {
     }
     load()
   }, [])
+
+  function getIcons(v: Vaccimon): IconDefinition[] {
+    if(v.vaccine in icons)
+      return icons[v.vaccine]
+    else
+      return [faSyringe]
+  }
+  function getCardClass(v: Vaccimon): string {
+    if(v.vaccine in classes)
+      return classes[v.vaccine]
+    else
+      return 'card-other'
+  }
 
   return (
     <>
@@ -50,17 +87,34 @@ export default function Home() {
           Your Vaccidex:
           <Container className={styles.previews}>
             {vaccimon && vaccimon.map(v =>
-              <div key={v.id} className={styles.preview}>
-                <Image src={v.avatarUrl} width={128} height={128} alt="" />
-                <div className={styles.name}>
+              <div key={v.id} className={`${styles.preview} ${getCardClass(v)}`}>
+                <span className={styles.name}>
                   {v.fullName}
+                </span>
+                <span className={styles.vaccinationKind}>
+                  {getIcons(v).map((icon, i) => <FontAwesomeIcon key={i} icon={icon} fixedWidth />)}
+                </span>
+                <div className={styles.imageContainer}>
+                  <Image src={v.avatarUrl} width={245} height={245} alt="" />
                 </div>
-                <div className={styles.type}>
-                  {v.vaccine}
-                  {' '}
-                  <Badge bg="primary">
-                    {v.isFullyVaccinated ? '2' : '1'}
-                  </Badge>
+                <div className={styles.properties}>
+                  <div>
+                    <span className={styles.propName}>Geburtstag</span>
+                    <span className={styles.propValue}>{v.dateOfBirth.toLocaleDateString()}</span>
+                  </div>
+                  <div>
+                    <span className={styles.propName}>Impfdatum</span>
+                    <span className={styles.propValue}>{v.vaccinationDate.toLocaleDateString()}</span>
+                  </div>
+                  <div>
+                    <span className={styles.propName}>Impfstoff</span>
+                    <span className={styles.propValue}>{v.vaccine}</span>
+                  </div>
+                  <div>
+                    <span className={styles.propName}>Vollständig</span>
+                    <span className={styles.propValue}><FontAwesomeIcon icon={v.isFullyVaccinated ? faCheck : faTimes} fixedWidth /></span>
+                  </div>
+                  <Button className={styles.showQRButton}>QR Code anzeigen</Button>
                 </div>
               </div>
             )}
