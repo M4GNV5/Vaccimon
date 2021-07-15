cdb2eed39b3070541fa1bfe62de76af2b13987a2
Alexander Horn <alexhorn@users.noreply.github.com>
2021-07-15 22:43:22 +0200

    Fix prod build failing due to eudgc.ts reference

--- vaccimon-app/lib/eudgc.d.ts
+++ vaccimon-app/lib/eudgc.d.ts
@@ -1,10 +1,5 @@
+// eudgc does not seem to have correct typings even though its written in ts
 declare module "eudgc" {
-	// XXX: eudgc does not seem to have correct typings even though its written in ts
-	import { EuDgcCert, EuDgcVaccincation } from 'eudgc/src/eudgc'
-	
-	export type EuDgcCert = EuDgcCert
-	export type EuDgcVaccincation = EuDgcVaccincation
-
 	declare global {
 		interface Window {
 			EuDgc_parse: (encodedData: string) => Promise<EuDgcCert>,
