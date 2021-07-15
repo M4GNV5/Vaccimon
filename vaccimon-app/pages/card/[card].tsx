b0bf815e8962933a9195e504f86d1b1d0373bc1c
Alexander Horn <alexhorn@users.noreply.github.com>
2021-07-15 22:25:17 +0200

    Add overview and swipable card view

---
+++ vaccimon-app/pages/card/[card].tsx
@@ -0,0 +1,155 @@
+import { useEffect, useMemo, useState } from 'react'
+import { useRouter } from 'next/router'
+import Head from 'next/head'
+import Image from 'next/image'
+import { Button, Container, Nav, Navbar } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  IconDefinition,
+  faChessPawn,
+  faCrow,
+  faDna,
+  faVirus,
+  faQrcode,
+  faSyringe,
+  faCheck,
+  faTimes,
+  faFlagUsa,
+  faTint,
+} from '@fortawesome/free-solid-svg-icons'
+import SwipableViews from 'react-swipeable-views'
+import VaccimonRepo from '../../lib/repository'
+import styles from '../../styles/card.module.css'
+import { Vaccimon } from '../../lib/vaccimon'
+
+const icons: {[key: string]: IconDefinition[]} = {
+  "Comirnaty": [faDna, faChessPawn],
+  "Spikevax": [faDna, faCrow],
+  "Vaxzevria": [faVirus, faTint],
+  "COVID-19 Vaccine Janssen": [faVirus, faFlagUsa],
+}
+const classes: {[key: string]: string} = {
+  "Comirnaty": styles["card-comirnaty"],
+  "Spikevax": styles["card-spikevax"],
+  "Vaxzevria": styles["card-vaxzevria"],
+  "COVID-19 Vaccine Janssen": styles["card-janssen"],
+}
+
+export default function Card() {
+  const router = useRouter()
+  const [vaccimon, setVaccimon] = useState<Vaccimon[]>()
+  const index = useMemo(() => {
+    if (!router.query || !vaccimon) {
+      return 0
+    }
+    const id = Buffer.from(router.query['card'] as string, 'base64').toString('binary')
+    console.log(id)
+    console.log(vaccimon)
+    const index = vaccimon.findIndex(x => x.id === id) ?? 0
+    console.log(index)
+    return 1
+  }, [router.query, vaccimon])
+
+  function getIcons(v: Vaccimon): IconDefinition[] {
+    if(v.vaccine in icons)
+      return icons[v.vaccine]
+    else
+      return [faSyringe]
+  }
+
+  function getCardClass(v: Vaccimon): string {
+    if(v.vaccine in classes)
+      return classes[v.vaccine]
+    else
+      return 'card-other'
+  }
+
+  function updateUrl(index: number): void {
+    if (!vaccimon) {
+      return
+    }
+
+    const id = vaccimon[index].id
+    router.replace(`/card/${Buffer.from(id, 'binary').toString('base64')}`, undefined, { shallow: true })
+  }
+
+  useEffect(() => {
+    async function load () {
+      const repo = new VaccimonRepo()
+      try {
+        await repo.open()
+        const certs = await repo.getAllCerts()
+        const vaccimon = await Promise.all(certs.map(x => Vaccimon.parse(x.data)))
+        
+        vaccimon.sort((a, b) => a.fullName.localeCompare(b.fullName))
+        setVaccimon(vaccimon)
+      } finally {
+        await repo.close()
+      }
+    }
+    load()
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <title>Vaccímon</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <>
+        <Navbar bg="light">
+          <Container>
+            <Navbar.Brand>Vaccimon</Navbar.Brand>
+            <Nav.Link href="/scan">
+              <FontAwesomeIcon icon={faQrcode} />
+              {' '}
+              Scan a Vaccimon
+            </Nav.Link>
+          </Container>
+        </Navbar>
+        <Container>
+          {vaccimon &&
+            <SwipableViews
+              className={styles.previews}
+              index={index}
+              onChangeIndex={updateUrl}
+            >
+              {vaccimon.map(v =>
+                <div key={v.id} className={`${styles.card} ${getCardClass(v)}`}>
+                  <span className={styles.name}>
+                    {v.fullName}
+                  </span>
+                  <span className={styles.vaccinationKind}>
+                    {getIcons(v).map((icon, i) => <FontAwesomeIcon key={i} icon={icon} fixedWidth />)}
+                  </span>
+                  <div className={styles.imageContainer}>
+                    <Image src={v.avatarUrl} width={245} height={245} alt="" />
+                  </div>
+                  <div className={styles.properties}>
+                    <div>
+                      <span className={styles.propName}>Geburtstag</span>
+                      <span className={styles.propValue}>{v.dateOfBirth.toLocaleDateString()}</span>
+                    </div>
+                    <div>
+                      <span className={styles.propName}>Impfdatum</span>
+                      <span className={styles.propValue}>{v.vaccinationDate.toLocaleDateString()}</span>
+                    </div>
+                    <div>
+                      <span className={styles.propName}>Impfstoff</span>
+                      <span className={styles.propValue}>{v.vaccine}</span>
+                    </div>
+                    <div>
+                      <span className={styles.propName}>Vollständig</span>
+                      <span className={styles.propValue}><FontAwesomeIcon icon={v.isFullyVaccinated ? faCheck : faTimes} fixedWidth /></span>
+                    </div>
+                    <Button className={styles.showQRButton}>QR Code anzeigen</Button>
+                  </div>
+                </div>
+              )}
+            </SwipableViews>
+          }
+        </Container>
+      </>
+    </>
+  )
+}
--- vaccimon-app/pages/index.tsx
+++ vaccimon-app/pages/index.tsx
@@ -1,37 +1,13 @@
+import { useEffect, useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
-import { Button, Container, Nav, Navbar } from 'react-bootstrap'
+import Link from 'next/link'
+import { Badge, Container, Nav, Navbar } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-  IconDefinition,
-  faChessPawn,
-  faCrow,
-  faDna,
-  faVirus,
-  faQrcode,
-  faSyringe,
-  faCheck,
-  faTimes,
-  faFlagUsa,
-  faTint,
-} from '@fortawesome/free-solid-svg-icons'
+import { faQrcode } from '@fortawesome/free-solid-svg-icons'
 import VaccimonRepo from '../lib/repository'
-import { useEffect, useState } from 'react'
-import styles from '../styles/index.module.css'
 import { Vaccimon } from '../lib/vaccimon'
-
-const icons: {[key: string]: IconDefinition[]} = {
-  "Comirnaty": [faDna, faChessPawn],
-  "Spikevax": [faDna, faCrow],
-  "Vaxzevria": [faVirus, faTint],
-  "COVID-19 Vaccine Janssen": [faVirus, faFlagUsa],
-}
-const classes: {[key: string]: string} = {
-  "Comirnaty": styles["card-comirnaty"],
-  "Spikevax": styles["card-spikevax"],
-  "Vaxzevria": styles["card-vaxzevria"],
-  "COVID-19 Vaccine Janssen": styles["card-janssen"],
-}
+import styles from '../styles/index.module.css'
 
 export default function Home() {
   const [vaccimon, setVaccimon] = useState<Vaccimon[]>()
@@ -53,74 +29,44 @@ export default function Home() {
     load()
   }, [])
 
-  function getIcons(v: Vaccimon): IconDefinition[] {
-    if(v.vaccine in icons)
-      return icons[v.vaccine]
-    else
-      return [faSyringe]
-  }
-  function getCardClass(v: Vaccimon): string {
-    if(v.vaccine in classes)
-      return classes[v.vaccine]
-    else
-      return 'card-other'
-  }
-
   return (
     <>
       <Head>
         <title>Vaccímon</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <>
-        <Navbar bg="light">
-          <Container>
-            <Navbar.Brand>Vaccimon</Navbar.Brand>
-            <Nav.Link href="/scan">
-              <FontAwesomeIcon icon={faQrcode} />
-              {' '}
-              Scan a Vaccimon
-            </Nav.Link>
-          </Container>
-        </Navbar>
+      <Navbar bg="light">
         <Container>
-          Your Vaccidex:
-          <Container className={styles.previews}>
-            {vaccimon && vaccimon.map(v =>
-              <div key={v.id} className={`${styles.preview} ${getCardClass(v)}`}>
-                <span className={styles.name}>
-                  {v.fullName}
-                </span>
-                <span className={styles.vaccinationKind}>
-                  {getIcons(v).map((icon, i) => <FontAwesomeIcon key={i} icon={icon} fixedWidth />)}
-                </span>
-                <div className={styles.imageContainer}>
-                  <Image src={v.avatarUrl} width={245} height={245} alt="" />
-                </div>
-                <div className={styles.properties}>
-                  <div>
-                    <span className={styles.propName}>Geburtstag</span>
-                    <span className={styles.propValue}>{v.dateOfBirth.toLocaleDateString()}</span>
-                  </div>
-                  <div>
-                    <span className={styles.propName}>Impfdatum</span>
-                    <span className={styles.propValue}>{v.vaccinationDate.toLocaleDateString()}</span>
-                  </div>
-                  <div>
-                    <span className={styles.propName}>Impfstoff</span>
-                    <span className={styles.propValue}>{v.vaccine}</span>
-                  </div>
-                  <div>
-                    <span className={styles.propName}>Vollständig</span>
-                    <span className={styles.propValue}><FontAwesomeIcon icon={v.isFullyVaccinated ? faCheck : faTimes} fixedWidth /></span>
-                  </div>
-                  <Button className={styles.showQRButton}>QR Code anzeigen</Button>
-                </div>
-              </div>
-            )}
-          </Container>
+          <Navbar.Brand>Vaccimon</Navbar.Brand>
+          <Nav.Link href="/scan">
+            <FontAwesomeIcon icon={faQrcode} />
+            {' '}
+            Scan a Vaccimon
+          </Nav.Link>
         </Container>
-      </>
+      </Navbar>
+      <Container>
+        Your Vaccidex:
+        <Container className={styles.previews}>
+          {vaccimon && vaccimon.map(v =>
+            <Link key={v.id} href={`/card/${Buffer.from(v.id, 'binary').toString('base64')}`} passHref>
+              <a className={styles.preview}>
+                <Image src={v.avatarUrl} width={128} height={128} alt="" />
+                <div className={styles.name}>
+                  {v.fullName}
+                </div>
+                <div className={styles.type}>
+                  {v.vaccine}
+                  {' '}
+                  <Badge bg="primary">
+                    {v.isFullyVaccinated ? '2' : '1'}
+                  </Badge>
+                </div>
+              </a>
+            </Link>
+          )}
+        </Container>
+      </Container>
     </>
   )
 }
