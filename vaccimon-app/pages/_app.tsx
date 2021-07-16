372772efe219ea578f346e3eb7490f4fb5530b0f
Alexander Horn <alexhorn@users.noreply.github.com>
2021-07-16 19:32:20 +0200

    Add workaround for iOS 14.6 IndexedDB bug

--- vaccimon-app/pages/_app.tsx
+++ vaccimon-app/pages/_app.tsx
@@ -3,6 +3,11 @@ import '@fortawesome/fontawesome-svg-core/styles.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+// workaround for this iOS 14.6 bug
+// https://bugs.webkit.org/show_bug.cgi?id=226547
+// eslint-disable-next-line no-unused-expressions
+typeof window !== 'undefined' && window.indexedDB
+
 function MyApp ({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
