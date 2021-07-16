e41778c3ea41697b76b093474cdc50116d6c4797
Alexander Horn <alexhorn@users.noreply.github.com>
2021-07-16 16:35:05 +0200

    Improve front page design

--- vaccimon-app/pages/index.tsx
+++ vaccimon-app/pages/index.tsx
@@ -1,7 +1,6 @@
 import { useMemo } from 'react'
-import { useRouter } from 'next/router'
 
-import { Container, ListGroup } from 'react-bootstrap'
+import { Container, Card, CardGroup } from 'react-bootstrap'
 
 import AppContainer from '../components/AppContainer'
 import AppNavbar from '../components/AppNavbar'
@@ -10,50 +9,53 @@ import useVaccimon from '../lib/repository-hook'
 import { achievements, calculateAchievements } from './achievements'
 
 import styles from '../styles/index.module.css'
+import Link from 'next/link'
 
 export default function Home () {
-  const router = useRouter()
   const vaccimon = useVaccimon()
   const unlockedAchievements = useMemo(() => calculateAchievements(vaccimon), [vaccimon])
 
   return (
     <AppContainer>
-      <AppNavbar title="Overview" />
+      <AppNavbar title="Dashboard" />
       <Container>
-        <br />
-        <ListGroup>
-          <ListGroup.Item action onClick={() => router.push('/vaccidex')}>
-            <div className="float-end">
-              {vaccimon.length}
-            </div>
-            <div>
-              <b>Your Vaccidex</b><br />
-              View all your catched Vaccímons and their Abilities
-            </div>
-          </ListGroup.Item>
-        </ListGroup>
-        <br />
-        <ListGroup>
-          <ListGroup.Item action onClick={() => router.push('/achievements')}>
-            <div className="float-end">
-              {`${unlockedAchievements.length} / ${achievements.length}`}
-            </div>
-            <div>
-              <b>Your Achievements</b><br />
-              View your achievement progress
-            </div>
-          </ListGroup.Item>
-        </ListGroup>
+        <CardGroup className={styles.cardGroup}>
+          <Link href="/vaccidex">
+            <Card className={styles.card}>
+              <Card.Body>
+                <Card.Title>
+                  Vaccimon
+                </Card.Title>
+                <Card.Text className={styles.cardText}>
+                  {vaccimon.length}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Link>
+          <Link href="/vaccidex">
+            <Card className={styles.card}>
+              <Card.Body>
+                <Card.Title>
+                Achievements
+                </Card.Title>
+                <Card.Text className={styles.cardText}>
+                  {`${unlockedAchievements.length} / ${achievements.length}`}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Link>
+        </CardGroup>
 
-        <div className={styles.disclaimer}>
-          <b>Please remember: </b>
-          This project is a parody and a fun way to show the downsides of
-          permanent non-changing vaccination certificates.<br />
-          This project is not associated with Nintendo, The Pokémon Company or
-          any government.<br />
-          This is a private fun project. You can find the source code on{' '}
-          <a href="https://github.com/M4GNV5/Vaccimon/" target="_blank" rel="noreferrer">Github</a>
-        </div>
+        <Container className={styles.disclaimer}>
+          <p>
+            This project is a <strong>parody</strong> and a fun way to show the downsides of permanent non-changing vaccination certificates.
+            It is <strong>not</strong> associated with Nintendo, The Pokémon Company or any government entity.
+          </p>
+          <p>
+            You can find the source code on{' '}
+            <a href="https://github.com/M4GNV5/Vaccimon/" target="_blank" rel="noreferrer">GitHub</a>.
+          </p>
+        </Container>
       </Container>
       <AppTabbar />
     </AppContainer>
